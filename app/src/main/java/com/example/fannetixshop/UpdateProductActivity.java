34ff6e537d6c90546be16d79239a498d45600f87
Meylin <meylinmoon@gmail.com>
2024-10-28 13:17:55 +0100

    Starting with the design of the activity: updateproduct

--- app/src/main/java/com/example/fannetixshop/UpdateProductActivity.java
+++ app/src/main/java/com/example/fannetixshop/UpdateProductActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class UpdateProductActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_update_product);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_upload_product), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
