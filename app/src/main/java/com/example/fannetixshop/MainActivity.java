5b5c4ff619135f2f92e8ac4eb227928961304dc4
Olaia <olaialor@gmail.com>
2024-11-05 09:57:09 +0100

    Retoques LogIn

--- app/src/main/java/com/example/fannetixshop/MainActivity.java
+++ app/src/main/java/com/example/fannetixshop/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.fannetixshop;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
